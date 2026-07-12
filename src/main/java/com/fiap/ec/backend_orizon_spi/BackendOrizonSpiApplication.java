32facc71fa4c85f1d604aeb6b21182fe1d65939d
Jhonatan <jhonatanlopessilva7@gmail.com>
2026-07-12 20:10:50 -0300

    chore: estrutura inicial gerada pelo Spring Initializr

---
+++ src/main/java/com/fiap/ec/backend_orizon_spi/BackendOrizonSpiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ec.backend_orizon_spi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOrizonSpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOrizonSpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/ec/backend_orizon_spi/BackendOrizonSpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ec.backend_orizon_spi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOrizonSpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
